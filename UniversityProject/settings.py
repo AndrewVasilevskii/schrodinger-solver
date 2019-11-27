566b864d7b3efb9f48de71b3c224a5209f4a7174
Andrew <andrew.vasilevskii@gmail.com>
2019-11-27 14:10:03 +0300

    added tichtext

--- UniversityProject/settings.py
+++ UniversityProject/settings.py
@@ -1,15 +1,25 @@
 import wx
-
+import wx.richtext as rt
 class Settings(wx.Frame):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.panel = wx.Panel()
+        self.panel = wx.Panel(self)
         self.mainSizer = mainSizer = wx.BoxSizer()
         self.panel.SetSizer(mainSizer)
 
-        self.gateDiameterPickerBox = wx.StaticBox(self.panel, label="Gate diameter: ")
+
+        self.gateDiameterPickerBox = wx.StaticBox(self.panel, label="--(Gate diameter)--: ")
         self.gateDiameterPickerSizer = wx.StaticBoxSizer(self.gateDiameterPickerBox, wx.HORIZONTAL)
-        self.gateDiameterPicker = wx.StaticText(self.panel, label="fdfdf")
+        self.gateDiameterPicker = wx.SpinCtrlDouble(self.panel, min=0.1, initial=0.1, inc=0.1)
         self.gateDiameterPickerSizer.Add(self.gateDiameterPicker)
-        mainSizer.Add(self.gateDiameterPickerBox)
+        k = rt.RichTextCtrl(self.panel,style=wx.TE_READONLY|wx.NO_BORDER)
+        k.SetBackgroundColour(self.panel.GetBackgroundColour())
+        k.AppendText("HOHO")
+
+        k.SetMinSize((100,100))
+
+        self.gateDiameterPickerSizer.Add(k)
+
+
+        mainSizer.Add(self.gateDiameterPickerSizer)
